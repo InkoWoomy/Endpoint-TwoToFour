def0b38016c21c662c39cc040c3d5d4a2045cc30
Alexander <yoda4115@gmail.com>
2025-01-07 15:18:01 -0800

    gitignore

--- obj/Debug/net8.0/Endpoint-TwoToFour.AssemblyInfo.cs
+++ obj/Debug/net8.0/Endpoint-TwoToFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Endpoint-TwoToFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2432152a9933c175d0ea63f9cf4c8aa2167f991e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Endpoint-TwoToFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Endpoint-TwoToFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
